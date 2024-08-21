20f66466eff7e7f678d3ea8e33ecc59d28bfef64
305develpment <305softwareengineering@gmail.com>
2024-08-20 22:27:59 -0500

    bug

--- task-tracker-frontend/src/index.js
+++ task-tracker-frontend/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import TaskApp from './App'; // Make sure the path is correct
+import App from './App'; // Importing the App component
 import reportWebVitals from './reportWebVitals';
 
-// Use TaskApp as the root component
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TaskApp />
+    <App /> {/* Render the App component */}
   </React.StrictMode>
 );
 
